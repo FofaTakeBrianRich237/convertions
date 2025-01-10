52f2b5b3d360865fbe673eba9de04c998a092759
FofaTakeBrianRich237 <kgetazoni@gmail.com>
2025-01-10 02:38:03 +0000

    save

--- header/display.h
+++ header/display.h
@@ -1,6 +1,9 @@
 #ifndef __DISPLAY_H_
 #define __DISPLAY_H_
 
-void main_menu();
+void upper_case(std::string word);
+void main_menu(MESURE * messure,int &num_messures);
+void display_messures(MESURE * messure,int &num_messures);
+void modifORuse (MESURE * messure,int &num_messures)
 
 #endif

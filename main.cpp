47f4509cf9ccbf3a56ea319107c9cd336b4d33a5
FofaTakeBrianRich237 <kgetazoni@gmail.com>
2025-01-23 20:08:50 +0100

    addition of text filles

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include "header/menu_sys.h"
 #include "header/mecanic.h"
-#include<fstream>
 
 int main()
 {
+  MESURE * messure;
+  int num_messures;
+  SPECIALMESURE * sp_messure;
+  int sp_num_messures;
+  main_menu(messure,num_messures,sp_messure,sp_num_messures);
   return 0;
 }
